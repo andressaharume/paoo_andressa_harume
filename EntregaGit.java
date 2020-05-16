588778737487114f09fcad6536115d0be1a2a3d1
global <global>
2020-05-16 15:02:04 -0300

    Agora estamos lendo o nome do usuario

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,6 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class EntregaGit {
 
 	public static void main( String ... args) {
 		System.out.println("Entregando tarefas com o Git na linha de comando...");
-	}
+		String nome = JOptionPane.showIputDialog("Qual seu nome?");
+		JOptionPane.showMessageDialog(null, String.format ("Oi, %s", nome));	}
 }
